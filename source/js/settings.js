5c62e7c6cfbb8254ccf445b38f6b4044051ece69
inthemoodforsuffering <firarps@proton.me>
2024-10-13 11:49:33 +0700

    1.0.3

--- source/js/settings.js
+++ source/js/settings.js
@@ -3,5 +3,5 @@ const sheetID = `1DGmsDi4Wgq4mWozc6Y8VhWszbL4r4kAQ-k3lRx6UWrQ`;
 const oldSheetID = ``;
 const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
-const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
+const threadTags = ["vital", "priority", "rapidfire", "long thread", "group plot", "sand in your eyes", "darkness hummed", "99 degrees", "romantic", "family", "friends", "coworkers"];
 const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
